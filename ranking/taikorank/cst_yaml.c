7a45f12fc4c4c23294d1fc18bab41aa93d6eb498
Maugere Lucas <maugere.lucas@gmail.com>
2015-12-19 18:44:52 +0100

    add density yaml file.

---
+++ ranking/taikorank/cst_yaml.c
@@ -0,0 +1,63 @@
+/*
+ *  Copyright (©) 2015 Lucas Maugère, Thomas Mijieux
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#include <stdio.h>
+
+#include "util/hashtable/hashtable.h"
+#include "util/list/list.h"
+#include "yaml/yaml2.h"
+
+#include "taiko_ranking_map.h"
+#include "stats.h"
+#include "cst_yaml.h"
+#include "print.h"
+
+//--------------------------------------------------
+
+double cst_f(struct hash_table * ht, const char * key)
+{
+  struct yaml_wrap * yw = NULL;
+  ht_get_entry(ht, key, &yw);
+  if(yw == NULL)
+    tr_error("Constant %s not found.", key);
+  else if(yw->type != YAML_SCALAR)
+    tr_error("Constant %s is not a scalar.", key);
+  else
+    return atof(yw->content.scalar);
+  return -1; 
+}
+
+struct stats * stats_get(struct hash_table * ht, const char * key)
+{
+  struct stats * stats = calloc(sizeof(*stats), 1);
+  struct yaml_wrap * yw = NULL;
+  ht_get_entry(ht, key, &yw);
+  if(yw == NULL)
+    tr_error("Stats %s not found.", key);
+  else if(yw->type != YAML_MAPPING)
+    tr_error("Stats %s is not a mapping.", key);
+  else
+    {
+      struct hash_table * ht_stats = yw->content.mapping;
+      stats->scaling = cst_f(ht_stats, "scaling");
+      stats->median  = cst_f(ht_stats, "median");
+      stats->mean    = cst_f(ht_stats, "mean");
+      stats->d1 = cst_f(ht_stats, "d1");
+      stats->d9 = cst_f(ht_stats, "d9");
+      stats->q1 = cst_f(ht_stats, "q1");
+      stats->q3 = cst_f(ht_stats, "q3");
+    }
+  return stats;
+}
