6fe1e2c1c7a18c53dae4782a33db0f717c80693b
Thomas Mijieux <thomas {dot} mijieux {@} hotmail {dot} fr>
2016-07-28 12:31:38 +0200

    dep install scrypt for msys2

---
+++ scripts/dep_install_msys2.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pacman -Syu mingw-w64-x86_64-toolchain mingw-w64-x86_64-libtool \
+	mingw-w64-x86_64-xz mingw-w64-x86_64-libyaml \
+	mingw-w64-x86_64-sqlite3 mingw-w64-x86_64-glib2 \
+	mingw-w64-x86_64-python2 mingw-w64-x86_64-openssl
