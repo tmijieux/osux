bcbda58aad43541c19bff1f57c531a1d3cf13618
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2016-08-14 22:21:28 +0200

    buildsystem editor

--- src/editor/editor.gresources.h
+++
@@ -1,7 +0,0 @@
-#ifndef __RESOURCE_editor_H__
-#define __RESOURCE_editor_H__
-
-#include <gio/gio.h>
-
-extern GResource *editor_get_resource (void);
-#endif
---
+++ src/lib/database/beatmap_db.sql.h
@@ -0,0 +1,2 @@
+extern const unsigned char _beatmap_db_data[];
+extern const unsigned long _beatmap_db_length;
