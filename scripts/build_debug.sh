ba2e3a2f408edf63b03ef0273079a0bafb33f73e
Thomas Mijieux <thomas.mijieux@hotmail.fr>
2016-08-30 23:54:38 +0200

    add Wall flag to configure script helper

--- scripts/build_debug.sh
+++ scripts/build_debug.sh
@@ -3,4 +3,4 @@
 PREFIX=${PREFIX:-$HOME/prefix}
 mkdir -p $PREFIX
 rootdir=$(dirname $0)/../
-$rootdir/configure --prefix=$PREFIX CFLAGS='-ggdb -DDEBUG -O0 -fsanitize=address -fsanitize=undefined' LDFLAGS='-fsanitize=address -fsanitize=undefined'
+$rootdir/configure --prefix=$PREFIX CFLAGS='-Wall -ggdb -DDEBUG=1 -O0 -fsanitize=address -fsanitize=undefined' LDFLAGS='-fsanitize=address -fsanitize=undefined'
